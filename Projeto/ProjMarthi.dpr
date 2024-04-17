6facb22e5c6dc32b2881039c42cd2c038c5783b3
SIG\Thiago.Barcellos <t.afonso@sig2000.com.br>
2024-04-17 14:44:11 -0300

    main

---
+++ Projeto/ProjMarthi.dpr
@@ -0,0 +1,16 @@
+program ProjMarthi;
+
+uses
+  Vcl.Forms,
+  CadProdutosFrm in '..\Forms\CadProdutosFrm.pas' {frmCadProdutos},
+  GeralDMFrm in '..\Forms\DataModules\GeralDMFrm.pas' {frmGeralDM: TDataModule};
+
+{$R *.res}
+
+begin
+  Application.Initialize;
+  Application.MainFormOnTaskbar := True;
+  Application.CreateForm(TfrmCadProdutos, frmCadProdutos);
+  Application.CreateForm(TfrmGeralDM, frmGeralDM);
+  Application.Run;
+end.
