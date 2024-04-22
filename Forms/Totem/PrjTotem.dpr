4af0e3bfbf213688c7a665377336f9187d52bd29
Marcalinfo <matheusmarcal.mma@gmail.com>
2024-04-22 15:39:42 -0300

    atualização totem

--- Forms/Totem/PrjTotem.dpr
+++ Forms/Totem/PrjTotem.dpr
@@ -3,12 +3,14 @@ program PrjTotem;
 uses
   System.StartUpCopy,
   FMX.Forms,
-  frmTotemPrincipal in 'frmTotemPrincipal.pas' {TotemPrincipalfrm};
+  frmTotemPrincipal in 'frmTotemPrincipal.pas' {TotemPrincipalfrm},
+  GeralDMFrm in '..\DataModules\GeralDMFrm.pas' {frmGeralDM: TDataModule};
 
 {$R *.res}
 
 begin
   Application.Initialize;
   Application.CreateForm(TTotemPrincipalfrm, TotemPrincipalfrm);
+  Application.CreateForm(TfrmGeralDM, frmGeralDM);
   Application.Run;
 end.
